d5cac36939226efa162bc355f7148a08340fd29e
Anastasia Kononosova <mrs.anastasia96@gmail.com>
2020-07-17 13:36:21 -0400

    first commit

---
+++ src/test/java/com/G6_Cybertek/runners/Cukes_Runner.java
@@ -0,0 +1,21 @@
+package com.G6_Cybertek.runners;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        plugin = {"html:target/cucumber-report.html",
+                "json:target/cucumber.json",
+                "rerun:target/rerun.txt"
+        },
+        features = "src/test/resources/features",
+        glue = "com/G6_Cybertek/step_definitions",
+        dryRun = false,
+        tags = ""
+
+)
+
+public class Cukes_Runner {
+}
